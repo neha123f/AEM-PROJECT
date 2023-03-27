ecb70e7f819158eab3e2bc04aa7d05be38d2ed45
Meghana R <meghana.r@valtech.com>
2023-03-27 10:33:09 +0530

    first AEM commit

---
+++ core/src/main/java/com/mysite/core/servlets/TestAgain.java
@@ -0,0 +1,22 @@
+//package com.mysite.core.servlets;
+//import com.day.cq.commons.jcr.JcrConstants;
+//import org.apache.sling.api.SlingHttpServletRequest;
+//import org.apache.sling.api.SlingHttpServletResponse;
+//import org.apache.sling.api.resource.Resource;
+//import org.apache.sling.api.servlets.HttpConstants;
+//import org.apache.sling.api.servlets.SlingAllMethodsServlet;
+//import org.apache.sling.api.servlets.SlingSafeMethodsServlet;
+//import org.apache.sling.servlets.annotations.SlingServletResourceTypes;
+//import org.osgi.service.component.annotations.Component;
+//import org.osgi.service.component.propertytypes.ServiceDescription;
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//import javax.servlet.Servlet;
+//import javax.servlet.ServletException;
+//import java.io.IOException;
+//import java.util.List;
+//import org.apache.sling.api.request.RequestParameter;
+//@SlingServlet(path="/bin/calculatorServlet", methods="POST")
+//public class TestAgain extends SlingAllMethodsServlet {
+//	private static final long serialVersionUID = 1L;
+//}
